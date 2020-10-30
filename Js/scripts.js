5c856ba02f38f6397a63070c95acffdfcc1f03d1
Fotsing237 <baudoinfotsing01824@gmail.com>
2020-10-30 11:34:59 +0100

    Create a Calculator

---
+++ Js/scripts.js
@@ -0,0 +1,17 @@
+console.log("Hello word");
+/*function value (valeur)
+{
+    console.log(valeur);
+    var button = document.querySelector(".button");
+button.addEventListener("click", ()=>{
+    console.log(this)
+});*/
+
+var array = [1,2,3,4]; 44
+var i=0;
+array.foreach(() =>{
+	i++;
+	var element=array[i];
+	console.log(element);
+	});
+
